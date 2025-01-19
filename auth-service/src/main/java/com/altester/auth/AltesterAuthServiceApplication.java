4e4c2dfcb7824ac11aa05a103b86d7b31b115870
Vezem1r <akulovichvanya@gmail.com>
2025-01-19 22:07:33 +0100

    Initial commit with multiple microservices

---
+++ auth-service/src/main/java/com/altester/auth/AltesterAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.altester.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltesterAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AltesterAuthServiceApplication.class, args);
+	}
+
+}
