8eeb4f9153fa4913ba9537163bb8e7b7fb8dcf58
Vezem1r <akulovichvanya@gmail.com>
2025-04-25 01:31:48 +0200

    Ai-grading-service init

---
+++ ai-grading-service/src/main/java/com/altester/ai_grading_service/AiGradingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.altester.ai_grading_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiGradingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiGradingServiceApplication.class, args);
+	}
+
+}
---
+++ ai-grading-service/src/test/java/com/altester/ai_grading_service/AiGradingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.altester.ai_grading_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiGradingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
