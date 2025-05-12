6c22be254cbb9bc696373b29171200edc6f8e6a3
Vezem1r <akulovichvanya@gmail.com>
2025-05-12 18:48:49 +0200

    returned webconfigs

---
+++ chat-service/src/main/java/com/altester/chat_service/config/WebConfig.java
@@ -0,0 +1,42 @@
+package com.altester.chat_service.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+  private final AppConfig appConfig;
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry
+        .addMapping("/**")
+        .allowedOrigins(appConfig.getAllowedOrigins().toArray(new String[0]))
+        .allowedMethods("GET", "POST", "PUT", "DELETE")
+        .allowedHeaders("*")
+        .allowCredentials(true)
+        .maxAge(3600);
+  }
+
+  @Bean
+  public CorsFilter corsConfigurationFilter() {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowCredentials(true);
+
+    appConfig.getAllowedOrigins().forEach(config::addAllowedOrigin);
+
+    config.addAllowedHeader("*");
+    config.addAllowedMethod("*");
+    source.registerCorsConfiguration("/**", config);
+    return new CorsFilter(source);
+  }
+}
