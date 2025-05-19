bb045d51575e8d9850ffce118d47ee9eaf517520
Vezem1r <akulovichvanya@gmail.com>
2025-05-19 19:43:44 +0200

    Create empty project for load tests

---
+++ loadtest/src/test/java/com/altester/loadtest/LoadtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.altester.loadtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
